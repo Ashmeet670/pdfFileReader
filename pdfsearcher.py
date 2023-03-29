e6f4fc8c76e09afad9f9640bdaae963a7da8dcaf
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2023-03-29 16:56:22 +0530

    QOL changes
    - added a reply to tell if no file was found with the requested text
    - changed some variable name for better understanding

--- pdfsearcher.py
+++ pdfsearcher.py
@@ -1,9 +1,11 @@
 import os
 from pypdf import PdfReader
 
-search_string = input("Enter the string to search: ")
+search_text = input("Enter the text to search: ")
 
-folder_path = "" #exact file adress of the downloaded certificate folder
+folder_path = "" #the exact path of the folder containing all the pdf files
+
+fileFound = False
 
 # Loop through all files in the folder
 for filename in os.listdir(folder_path):
@@ -12,7 +14,9 @@ for filename in os.listdir(folder_path):
             pdf_reader = PdfReader(pdf_file)
             for page in range(len(pdf_reader.pages)):
                 page_text = pdf_reader.pages[page].extract_text()
-                if search_string.lower() in page_text.lower():
+                if search_text.lower() in page_text.lower():
                     print(f"{filename}, page {page+1}")
-
-                    
+                    fileFound = True
+                
+if(fileFound == False):
+   print("no such file with the provided text")
